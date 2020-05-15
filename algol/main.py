7024ed2ae1e134b7093190f2a65d7b384522a5fb
Michał Sieroń <michalwsieron@gmail.com>
2020-05-15 12:41:41 +0200

    Ray tracing! Some user interaction with mouse and keyboard

--- algol/main.py
+++ algol/main.py
@@ -1,13 +1,18 @@
 import os
 import math
 from pathlib import Path
+from random import uniform
 
 import moderngl as mgl
 import moderngl_window as mglw
+import pyglet
 import numpy as np
+from pyrr import Matrix33
 
 from utils import shader_source
 from world import World, Star
+
+instance = None
 
 
 class App(mglw.WindowConfig):
@@ -19,18 +24,41 @@ class App(mglw.WindowConfig):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        global instance
+        instance = self
+
         self._path = Path(os.path.dirname(__file__))
 
         W, H = App.window_size
         self._world = World()
         self._world.add(
-            Star(200, x=(W / 2), y=(H / 2), z=0),
             Star(
-                100,
-                x=(lambda s, t, d: W / 3 * math.sin(t) + W / 2),
-                y=(H / 2),
-                z=(lambda s, t, d: 100 * math.cos(t)),
+                2,
+                x=(lambda s, t, d: 0),
+                y=(lambda s, t, d: 0),
+                z=(lambda s, t, d: 0),
+                color=(1.0, 1.0, 1.0),
+            ),
+            Star(
+                1,
+                x=(lambda s, t, d: 8 * math.sin(t)),
+                y=(lambda s, t, d: 0),
+                z=(lambda s, t, d: 10 * math.cos(t)),
                 color=(1.0, 1.0, 0.0),
+            ),
+            Star(
+                0.5,
+                x=(lambda s, t, d: 16 * math.sin(t * 0.9)),
+                y=(lambda s, t, d: math.cos(t)),
+                z=(lambda s, t, d: 16 * math.cos(t * 0.9)),
+                color=(0.0, 1.0, 1.0),
+            ),
+            Star(
+                0.2,
+                x=(lambda s, t, d: 4 * math.sin(t * 1.5)),
+                y=0,
+                z=(lambda s, t, d: 4 * math.cos(t * 1.5)),
+                color=(1.0, 0.0, 1.0),
             ),
         )
 
@@ -47,22 +75,46 @@ class App(mglw.WindowConfig):
             {"NUMBER_OF_OBJECTS": self._world.size},
         )
         self.compute = self.ctx.compute_shader(compute_source)
+        self.perspective_matrix = Matrix33(
+            [[1, 0, 0], [0, 1, 0], [0, 0, 1]], dtype="f4"
+        )
+        self.camera_position = (0, 0, 20)
+        self.zoom_level = 1
 
     def render(self, time, frame_time):
         self._world.update(time)
 
-        w, h = self.texture.size
-        gw, gh = 16, 16
-        nx, ny, nz = w // gw, h // gh, 1
+        W, H = self.texture.size
 
         self.compute["background_color"] = (0.05, 0.05, 0.15)
         self.compute["objects"] = self._world.as_tuples()
         self.compute["colors"] = self._world.colors()
+        self.compute["perspective_matrix"].write(self.perspective_matrix)
+        self.compute["camera_position"] = self.camera_position
+        self.compute["zoom_level"] = self.zoom_level
 
-        self.texture.bind_to_image(0, read=False, write=True)
-        self.compute.run(nx, ny, nz)
+        self.compute.run(W, H, 1)
         self.texture.use(location=0)
+        self.texture.bind_to_image(0, read=False, write=True)
         self.quad_fs.render(self.quad_program)
+
+    def key_event(self, key, action, modifiers):
+        if action == self.wnd.keys.ACTION_RELEASE:
+            if key == self.wnd.keys.Y:
+                self.perspective_matrix = Matrix33(
+                    [[1, 0, 0], [0, 1, 0], [0, 0, 1]], dtype="f4"
+                )
+                self.camera_position = (0, 0, 20)
+            elif key == self.wnd.keys.Z:
+                self.perspective_matrix = Matrix33(
+                    [[1, 0, 0], [0, 0, 1], [0, 1, 0]], dtype="f4"
+                )
+                self.camera_position = (0, 0, 20)
+            elif key == self.wnd.keys.SPACE:
+                self.zoom_level = 1
+
+    def mouse_scroll_event(self, x_offset, y_offset):
+        self.zoom_level = min(max(self.zoom_level + y_offset / 10, 0), 10)
 
     @classmethod
     def run(cls):
--- algol/shaders/compute.glsl
+++ algol/shaders/compute.glsl
@@ -2,54 +2,98 @@
 
 #define NUMBER_OF_OBJECTS %%NUMBER_OF_OBJECTS%%
 
-layout (local_size_x=16, local_size_y=16) in;
+layout (local_size_x=1, local_size_y=1) in;
 
-layout (rgba8, location=0) writeonly uniform image2D destTex;
+layout (rgba8, location=0) writeonly uniform image2D out_tex;
 
 uniform vec4 objects[NUMBER_OF_OBJECTS];
 uniform vec3 colors[NUMBER_OF_OBJECTS];
 uniform vec3 background_color;
-
-bool inCircle(in vec2 pos, in vec4 circle) {
-    vec2 dist = circle.xy - pos;
-    return dot(dist, dist) < pow(circle.w, 2.0);
-}
+uniform mat3 perspective_matrix;
+uniform vec3 camera_position;
+uniform float zoom_level;
 
 float solarIntensity(in float dist, in float radius) {
     float c = sqrt(1 - dist/radius);
     return 0.3 + 0.93 * c - 0.23 * c * c;
 }
 
-float brightness(in vec3 color) {
-    return sqrt(dot(color * vec3(0.299, 0.587, 0.114), vec3(1.0)));
+bool sphere_hit(in vec3 center, in float radius,
+                in vec3 ray_origin, in vec3 ray_direction,
+                in float t_min, in float t_max,
+                out vec3 point, out float t) {
+
+    vec3 omc = ray_origin - center;
+    float b = dot(ray_direction, omc);
+    float c = dot(omc, omc) - radius * radius;
+    float bsqmc = b * b - c;
+    if (bsqmc > 0.0) {
+        float root = sqrt(bsqmc);
+        float temp = -b - root;
+        if (t_min < temp && temp < t_max) {
+            t = temp;
+            point = ray_origin + t * ray_direction;
+            return true;
+        }
+        temp = -b + root;
+        if (t_min < temp && temp < t_max) {
+            t = temp;
+            point = ray_origin + t * ray_direction;
+            return true;
+        }
+    }
+    return false;
 }
 
 void main() {
-    ivec2 texelPos = ivec2(gl_GlobalInvocationID.xy);
 
-    int closest = -1;
-    for (uint i = 0; i < NUMBER_OF_OBJECTS; i++) {
-        if (inCircle(texelPos, objects[i])) {
-            if (closest != -1) {
-                if (objects[closest].z > objects[i].z) {
-                    closest = int(i);
-                }
-            }
-            else {
-                closest = int(i);
-            }
-        }
-    }
+    vec2 max_xy = vec2(16.0, 9.0);
+    ivec2 texel_pos = ivec2(gl_GlobalInvocationID.xy);
+
+    ivec2 size = imageSize(out_tex);
     vec3 color = background_color;
 
-    if (closest != -1) {
-        float r = objects[closest].w;
-        float dist = distance(objects[closest].xy, texelPos);
-        color = solarIntensity(dist, r) * colors[closest];
+    if (
+        ((texel_pos.x / 80) % 2 == 0 && (texel_pos.y / 80) % 2 == 1) ||
+        ((texel_pos.x / 80) % 2 == 1 && (texel_pos.y / 80) % 2 == 0)) {
+        color = (vec3(1.0) - background_color) / 2;
     }
+
+    vec2 xy = (texel_pos * 2.0 - size) / size;
+    vec3 ray_origin = vec3(xy * max_xy, 0.0);
+    vec3 ray_direction = vec3(0.0, 0.0, -1.0);
+
+    float t_min = 0;
+    float t_max = 1.0 / 0.0;
+    vec3 hit_point;
+    float hit_t;
+    int hit_anything = -1;
+    float closest_so_far = t_max;
+
+    mat3 rotation = mat3(vec3(1, 0, 0), vec3(0, 0, 1), vec3(0, 1, 0));
+
+    for (int i = 0; i < NUMBER_OF_OBJECTS; i++) {
+        if (sphere_hit(zoom_level * (perspective_matrix * objects[i].xyz
+                                     - camera_position),
+                       zoom_level * objects[i].w,
+                       ray_origin, ray_direction,
+                       t_min, closest_so_far,
+                       hit_point, hit_t)) {
+            hit_anything = i;
+            closest_so_far = hit_t;
+        }
+    }
+
+    if (hit_anything >= 0) {
+        float dist = length(zoom_level * (perspective_matrix * objects[hit_anything].xyz
+                            - camera_position).xy - hit_point.xy);
+        color = solarIntensity(dist, zoom_level * objects[hit_anything].w)
+                * colors[hit_anything];
+    }
+
     imageStore(
-        destTex,
-        texelPos,
+        out_tex,
+        texel_pos,
         vec4(color, 1.0)
     );
 }
