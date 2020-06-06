ab080999b355a183122b29594ddd8104f3383294
Michał Sieroń <michalwsieron@gmail.com>
2020-06-06 15:46:37 +0200

    Added some comments

--- algol/logger.py
+++ algol/logger.py
@@ -3,11 +3,14 @@ import time
 
 
 class Logger:
-    def __init__(self, path):
-        self._path = os.path.join(path, "algol.log")
+    """Helper class for logging events"""
+
+    def __init__(self, path: str, name: str):
+        self._path = os.path.join(path, name)
         self._log_file = open(self._path, "a")
 
-    def log(self, msg):
+    def log(self, msg: str):
+        """Use this method to log given message"""
         self._log_file.write(f"{time.time()};{msg}\n")
 
     def close(self):
